45040597339367297f9354884239995dc9edfadf
Nicolas Attalides <nicolas.attalides@gmail.com>
2021-06-21 23:26:00 +0100

    add setup file

---
+++ setup.R
@@ -0,0 +1,11 @@
+install.packages(c("tidyverse", "tidytext", "textdata"))
+
+install.packages("devtools")
+devtools::install_github("lchiffon/wordcloud2")
+
+library(tidyverse)
+library(tidytext)
+library(textdata)
+library(wordcloud2)
+
+sessionInfo()
